6e5989886f9ad74723b5359511495fcc9cc03924
Cho-Gath <ethan.m.winer@gmail.com>
2022-07-05 20:57:18 -0400

    backend works with json responses but needs to be change to work with post requests

--- backend/translator/urls.py
+++ backend/translator/urls.py
@@ -2,11 +2,13 @@ from django.urls import include, path
 
 from rest_framework import routers
 
-from translator.views import translate_english
+from translator.views import translate_to_english, translate_to_latin
+
 
 # router = routers.DefaultRouter()
 # router.register('test', test)
 
 urlpatterns = [
-  path('translate-english', translate_english)
+  path('translate-to-latin', translate_to_latin),
+  path('translate-to-english', translate_to_english)
 ]
--- backend/translator/views.py
+++ backend/translator/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
+import subprocess
 
 # Create your views here.
-def translate_english(request):
+def translate_to_latin(request):
   words = request.headers['Words'].split(',')
-  return JsonResponse({'translationList' : words});
+  translations = []
+  for word in words:
+    translation= subprocess.getoutput('/usr/local/words-1.97Ed/latin.sh ~E ' + word)
+    translations.append(translation)
+  return JsonResponse({'translationList': translations})
+
+def translate_to_english(request):
+  words = request.headers['Words'].split(',')
+  translations=[]
+  for word in words:
+    translation = subprocess.getoutput('echo "Enter" | /usr/local/words-1.97Ed/latin.sh ' + word)
+    translations.append(translation)
+  return JsonResponse({'translationList': translations})
